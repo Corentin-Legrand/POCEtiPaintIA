6bafeecffc4ab87ebacfba518344c12713e7c0c0
MathisDeSousa <157818653+mathis-de-sousa-pro@users.noreply.github.com>
2025-01-14 23:33:53 +0100

    [UPDATE] rajout d'un bouton pour close l'application et correction graphique

--- View/mainWindow.py
+++ View/mainWindow.py
@@ -8,15 +8,22 @@ class MainWindow(tk.Tk):
         self.configure(bg='white')
         self.chart_component = None
         self.canva_component = None
-        self.title("Phone Format Window")
+        self.title("POCETIPAINTIA")
         self.geometry("400x700")  # Adjusted dimensions to better fit the components
 
     def launch(self):
         self.mainloop()
 
+
     def addComponent(self, canvaComponent, chartComponent):
-        clear_button = tk.Button(self, text="Clear", command=self.handle_clear_button)
-        clear_button.pack(pady=10, expand=True, fill=tk.BOTH)
+        button_frame = tk.Frame(self, bg='white')
+        button_frame.pack(pady=10, fill=tk.X)
+
+        clear_button = tk.Button(button_frame, text="Clear", command=self.handle_clear_button)
+        clear_button.pack(side=tk.LEFT, padx=5, pady=5, expand=True, fill=tk.X)
+
+        close_button = tk.Button(button_frame, text="Close", command=self.quit)
+        close_button.pack(side=tk.LEFT, padx=5, pady=5, expand=True, fill=tk.X)
 
         self.canva_component = canvaComponent
         self.canva_component.pack(pady=10, expand=True, fill=tk.BOTH)
